b37dc4e5eac791e0ab7401e8bd5a9af0a9dc293e
joshua360x <jkw5499@psu.edu>
2022-05-23 08:06:27 -0400

    built out routes + private routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,36 @@
+import { Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import { AuthProvider } from "./context/AuthContext";
+import Home from "./views/Home";
+import Login from "./views/Login";
+import Post from "./views/Post";
+import ViewEverything from "./views/ViewEverything";
+
 export default function App() {
-  return <h1>Hello World</h1>;
+
+
+  return <main>
+
+  <AuthProvider>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/login'>
+          <Login />
+        </Route>
+        <PrivateRoute path='/post'>
+          <Post />
+        </PrivateRoute>
+        <PrivateRoute path='/all'>
+        <ViewEverything />
+        </PrivateRoute>
+      </Switch>
+    </Router>
+  </AuthProvider>
+
+  </main>;
 }
